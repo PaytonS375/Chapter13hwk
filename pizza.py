07ce707504e84a725eedc879ee77e3b856caed64
Payton <payton.sanislow@gmail.com>
2022-10-11 21:31:18 -0600

    final

--- pizza.py
+++ pizza.py
@@ -1,24 +1,4 @@
-'''
-Design a creative UI using Python's tkinter module to calculate the total cost of a pizza.
-The UI should have (at least) each widget that was covered in class:
-
-    Frames
-    Labels
-    input box
-    buttons
-    radio buttons
-    check boxes
-
-You can use check boxes for for selecting toppings (each with a different cost),
-radio buttons for the type of crust selected (each with a different cost), buttons for calculation and quit.
-
-The input box can be used to record the name of the person placing the order.
-
-Use a message box to display the total cost of the pizza along with the name of the person placing the order.
-
-Make sure your design is well laid out and intuitive to the user. Take account of spacing and packing so that everything is properly aligned and professional looking. Be creative with font, color, size, etc.
-'''
-
+from msilib.schema import RadioButton
 import tkinter
 import tkinter.messagebox
 
@@ -28,13 +8,13 @@ class JB_Pizza:
 
         self.main_window = tkinter.Tk()
 
-        self.shop_info_frame = tkinter.frame(self.main_window)
-        self.order_name_frame = tkinter.frame(self.main_window)
-        self.pizza_size_frame = tkinter.frame(self.main_window)
-        self.crust_type_frame = tkinter.frame(self.main_window)
-        self.sauce_type_frame = tkinter.frame(self.main_window)
-        self.toppings_frame = tkinter.frame(self.main_window)
-        self.command_buttons_frame = tkinter.frame(self.main_window)
+        self.shop_info_frame = tkinter.Frame(self.main_window)
+        self.order_name_frame = tkinter.Frame(self.main_window)
+        self.pizza_size_frame = tkinter.Frame(self.main_window)
+        self.crust_type_frame = tkinter.Frame(self.main_window)
+        self.sauce_type_frame = tkinter.Frame(self.main_window)
+        self.toppings_frame = tkinter.Frame(self.main_window)
+        self.command_buttons_frame = tkinter.Frame(self.main_window)
 
 
         self.shop_name = tkinter.Label(self.shop_info_frame, text='JB Pizza Shop', font=('Times New Roman',20,'bold underline'), fg='blue')
@@ -43,7 +23,7 @@ class JB_Pizza:
         self.shop_info_frame.pack(side='top')
 
 
-        self.prompt_name_label = tkinter.Label(self.order_frame, text='Name for Order: ', font=('Times New Roman',10,'bold'))
+        self.prompt_name_label = tkinter.Label(self.order_name_frame, text='Name for Order: ', font=('Times New Roman',10,'bold'))
         self.name_entry = tkinter.Entry(self.order_name_frame, width=20)
 
         self.prompt_name_label.pack(side='left')
@@ -53,7 +33,6 @@ class JB_Pizza:
 
         self.pizza_size = tkinter.Label(self.pizza_size_frame, text='Pizza Size: ', font=('Times New Roman',10,'bold'))
         self.pizza_size_var = tkinter.IntVar()
-        self.pizza_size_var.set(0)
 
         self.small = tkinter.Radiobutton(self.pizza_size_frame, text='Small: $10', variable=self.pizza_size_var, value=10)
         self.medium = tkinter.Radiobutton(self.pizza_size_frame, text='Medium: $14', variable=self.pizza_size_var, value=14)
@@ -72,7 +51,6 @@ class JB_Pizza:
 
         self.crust = tkinter.Label(self.crust_type_frame, text='Crust Type: ', font=('Times New Roman',10,'bold'))
         self.crust_var = tkinter.IntVar()
-        self.crust_var.set(0)
 
         self.regular = tkinter.Radiobutton(self.crust_type_frame, text='Regular: $0', variable=self.crust_var, value=0)
         self.thin = tkinter.Radiobutton(self.crust_type_frame, text='Thin: $2', variable=self.crust_var, value=2)
@@ -89,7 +67,6 @@ class JB_Pizza:
 
         self.sauce = tkinter.Label(self.sauce_type_frame, text='Sauce Type: ', font=('Times New Roman',10,'bold'))
         self.sauce_var = tkinter.IntVar()
-        self.sauce_var.set(0)
         self.marinara = tkinter.Radiobutton(self.sauce_type_frame, text='Marinara: $0', variable=self.sauce_var, value=0)
         self.white = tkinter.Radiobutton(self.sauce_type_frame, text='White: $2', variable=self.sauce_var, value=2)
         self.buffalo = tkinter.Radiobutton(self.sauce_type_frame, text='Buffalo: $4', variable=self.sauce_var, value=4)
@@ -103,7 +80,7 @@ class JB_Pizza:
         self.sauce_type_frame.pack()
 
 
-        self.toppings = tkinter.Label(self.toppings_frame, text='Select Toppings: ', font=('Times New Roman',10,'bold'))
+        self.toppings_label = tkinter.Label(self.toppings_frame, text='Select Toppings: ', font=('Times New Roman',10,'bold'))
         self.cheese = tkinter.IntVar()
         self.pepperoni = tkinter.IntVar()
         self.sausage = tkinter.IntVar()
@@ -116,11 +93,11 @@ class JB_Pizza:
         self.bacon.set(0)
         self.onions.set(0)
 
-        self.cheese = tkinter.Checkbutton(self.toppings_frame, text='Cheese: $0', variable=self.cheese, value=0)
-        self.pepperoni = tkinter.Checkbutton(self.toppings_frame, text='Pepperoni: $2', variable=self.pepperoni, value=2)
-        self.sausage = tkinter.Checkbutton(self.toppings_frame, text='Sausage: $3', variable=self.sausage, value=3)
-        self.bacon = tkinter.Checkbutton(self.toppings_frame, text='Bacon: $4', variable=self.bacon, value=4)
-        self.onions = tkinter.Checkbutton(self.toppings_frame, text='Onions: $1', variable=self.onions, value=1)
+        self.cheese = tkinter.Checkbutton(self.toppings_frame, text='Cheese: $0', variable=self.cheese)
+        self.pepperoni = tkinter.Checkbutton(self.toppings_frame, text='Pepperoni: $2', variable=self.pepperoni)
+        self.sausage = tkinter.Checkbutton(self.toppings_frame, text='Sausage: $3', variable=self.sausage)
+        self.bacon = tkinter.Checkbutton(self.toppings_frame, text='Bacon: $4', variable=self.bacon)
+        self.onions = tkinter.Checkbutton(self.toppings_frame, text='Onions: $1', variable=self.onions)
 
         self.toppings_label.pack()
 
@@ -132,8 +109,33 @@ class JB_Pizza:
 
         self.toppings_frame.pack()
 
+        def order_complete():
+        
+            total = 0
 
-        self.complete_order = tkinter.Button(self.command_buttons_frame, text='Order Complete', font=('Times New Roman',10,'bold'))
+            RadioButtonTotal = (self.pizza_size_var.get() + self.crust_var.get() + self.sauce_var.get())
+
+            CheckboxTotal = 0
+
+            if self.cheese == 1:
+                CheckboxTotal += 0
+
+            if self.pepperoni == 1:
+                CheckboxTotal += 2
+
+            if self.sausage == 1:
+                CheckboxTotal += 3
+
+            if self.bacon == 1:
+                CheckboxTotal += 4
+
+            if self.onions == 1:
+                CheckboxTotal += 1
+
+            tkinter.messagebox.showinfo('Order Summary', 'Name: ' + self.name_entry.get() + '\nTotal: $' + str(RadioButtonTotal + CheckboxTotal))
+
+
+        self.complete_order = tkinter.Button(self.command_buttons_frame, text='Order Complete', font=('Times New Roman',10,'bold'), command=order_complete)
         self.quit_button = tkinter.Button(self.command_buttons_frame, text='Quit', command=self.main_window.destroy, font=('Times New Roman',10,'bold'))
 
         self.complete_order.pack(side='left')
@@ -144,23 +146,4 @@ class JB_Pizza:
         tkinter.mainloop()
 
 
-    def order_complete(self):
-
-        total = (self.pizza_size_var.get() + self.crust_var.get() + self.sauce_var.get() +
-                self.cheese.get()*0 + self.pepperoni.get()*2 + self.sausage.get()*3 + self.bacon.get()*4 + self.onions.get()*1)
-
-        tkinter.messagebox.showinfo('Order Summary', 'Name: ' + self.name_entry.get() + '\nTotal: $' + str(total))
-
-        tkinter.mainloop()
-
-
 Pizza = JB_Pizza()
-
-print('moving on...')
-
-
-
-
-
-
-
